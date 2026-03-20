2d582e6e548e0bb4412c5509e13ea508c9ba97d3
YashSinghal02 <yash20singhal@gmail.com>
2026-03-20 14:23:36 +0530

    add render link in axios instance

--- src/Utils/axiosConfig.js
+++ src/Utils/axiosConfig.js
@@ -13,7 +13,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://job-portal-backend-6j6e.onrender.com",
   withCredentials: true, // IMPORTANT for cookies
 });
 
