2f83a16674100d19a1ef862c0c017e34c390b9bc
YashSinghal02 <yash20singhal@gmail.com>
2026-03-21 09:50:23 +0530

    add /api in instance

--- src/Utils/axiosConfig.js
+++ src/Utils/axiosConfig.js
@@ -13,7 +13,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL: "https://job-portal-backend-6j6e.onrender.com",
+    baseURL: "https://job-portal-backend-6j6e.onrender.com/api",
   // baseURL: "http://localhost:3000/api",
   withCredentials: true, // IMPORTANT for cookies
 });
